cf1f9cb51712ba59a64085383e0cce056f726bf0
Aleksandr Vyshnyvetskyi <vishniveckijj1994@gmail.com>
2022-11-11 18:02:12 +0200

    Up

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,4 @@
+import { Oval } from 'react-loader-spinner';
 import Phonebook from "./phonebook/Phonebook";
 import PhonebookFilter from './phonebook/PhonebookFilter';
 import PhonebookList from './phonebook/PhonebookList';
@@ -13,6 +14,15 @@ import './phonebook/Phonebook.css';
             <PhonebookFilter />
             <PhonebookList/>
           </>
+          <Oval
+          ariaLabel="loading-indicator"
+          height={150}
+          width={150}
+          strokeWidth={2}
+          strokeWidthSecondary={5}
+          color="#ffa200"
+          secondaryColor="#8bd3e1"
+        />
       </>
     );
    }
--- src/components/phonebook/PhonebookList.js
+++ src/components/phonebook/PhonebookList.js
@@ -1,5 +1,6 @@
-import { removeContact } from 'redux/contacts/contactsSlice';
 import { useSelector, useDispatch } from 'react-redux';
+import Notiflix from 'notiflix';
+import { removeContact } from 'redux/contacts/contactsSlice';
 import filterContact from 'redux/contacts/contactsSelector';
 import man from './img/man.jpg';
 import woman from './img/woman.jpg';
@@ -15,12 +16,22 @@ export default function PhonebookList() {
       return <img src={nogender} alt="contact-icon" className="gender-icon" />;
     }
   }
+  const removeMessage = () => {
+    return Notiflix.Report.failure('Remove', 'Contact removed', 'Close', {
+      svgSize: '200px',
+      titleFontSize: '24px',
+      messageFontSize: '20px',
+      buttonFontSize: '16px',
+      width: '300px',
+      backOverlay: true,
+      backOverlayClickToClose: true,
+    });
+  };
   const dispatch = useDispatch();
   const itemList = useSelector(filterContact);
   const oneItemPhonebook = itemList.map(({ id, name, number, gender }) => {
     return (
       <li key={id} className="list-item">
-        {' '}
         {icon(gender)}
         <p>
           {name}: {number}
@@ -28,9 +39,8 @@ export default function PhonebookList() {
         <button
           className="delete-btn"
           type="button"
-          onClick={() => dispatch(removeContact(id))}
+          onClick={() => dispatch(removeContact(id), removeMessage())}
         >
-          {' '}
           Delete
         </button>
       </li>
